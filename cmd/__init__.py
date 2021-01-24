0291e49e034d0348cc6b2dd1e2bcdb27e36e4211
id-shiv <id.shiv.prasad@gmail.com>
2021-01-24 21:48:01 +0530

    install msi method

--- cmd/__init__.py
+++ cmd/__init__.py
@@ -1,6 +1,12 @@
 # import required packages
+import subprocess
 
 def run(command: str):
     """ Run a dos command and get the output
     """
-    
+    pass
+
+def __install_msi(msi_path: str):
+    """ Install MSI and block execution until it completes
+    """
+    subprocess.call(f"msiexec /i {msi_path} /qn", shell=True)
